bd52de538cd6b09dea8a0e71d194dbea09148075
Ebad Ansari <ebadansari414@gmail.com>
2023-11-09 23:50:51 +0530

    'InfiniteScroller' component is created sucessfully. Minor bugs fixed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,40 @@
-import Layout from "@layout/LayoutManager";
-import MediaQuery from "./MediaQuery";
-import Test from "@components/Test";
-import Spinner from "@components/Spinner";
-import LoadingSpinner from "@components/icons/LoadingSpinner";
-import FixedList from "@components/InfiniteScroller";
-import MovieList from "@components/MovieList";
-import { Chance } from "chance";
+import InfiniteScroller from "@components/InfiniteScroller";
+import { TrailerModalProps } from "@interfaces/ModalInterface";
 import { Genre } from "@interfaces/TMDBGenre";
-import {
-	TrailerModalInputs,
-	TrailerModalProps,
-} from "@interfaces/ModalInterface";
-import { Prettify } from "@interfaces/interface";
-import { PropsWithChildren } from "react";
+import { Chance } from "chance";
 
-function Wrap({ children }: PropsWithChildren) {
-	return children;
-}
-
-function createMovieNode(movieInfo: TrailerModalInputs) {
-	const movieInfoDiv = document.createElement("div");
-	movieInfoDiv.className = "movie-info";
-
-	const movieImage = document.createElement("img");
-	movieImage.className = "movie-image";
-	movieImage.alt = "movieInfo.imageSource";
-	movieImage.src = movieInfo.imageSource;
-
-	const movieTitleDiv = document.createElement("div");
-	movieTitleDiv.className = "movie-title-div";
-
-	const movieTitleP = document.createElement("p");
-	movieTitleP.className = "movie-title-p";
-	movieTitleP.appendChild(document.createTextNode(movieInfo.title));
-
-	const extraButtonWrapperDiv = document.createElement("div");
-	extraButtonWrapperDiv.className = "extra-button-wrapper-div";
-
-	const extraButtonInnerDiv = document.createElement("div");
-	extraButtonInnerDiv.className = "extra-button-inner-div";
-
-	const extraButtonLeftDiv = document.createElement("div");
-	extraButtonLeftDiv.className = "extra-button-left-div";
-
-	const infoIconImg = document.createElement("img");
-	infoIconImg.className = "info-icon-img";
-	infoIconImg.src = "/public/icons/info-icon.svg";
-
-	const extraButtonRightDiv = document.createElement("div");
-	extraButtonRightDiv.className = "extra-button-right-div";
-
-	extraButtonRightDiv.onclick = function () {
-		if (movieInfo.liked) movieInfo.liked = false;
-		else movieInfo.liked = undefined;
-	};
-
-	movieInfoDiv.appendChild(movieImage);
-	movieInfoDiv.appendChild(movieTitleDiv);
-	movieInfoDiv.appendChild(extraButtonWrapperDiv);
-
-	movieTitleDiv.appendChild(movieTitleP);
-
-	extraButtonWrapperDiv.appendChild(extraButtonInnerDiv);
-
-	extraButtonInnerDiv.appendChild(extraButtonLeftDiv);
-	extraButtonInnerDiv.appendChild(extraButtonRightDiv);
-
-	extraButtonLeftDiv.appendChild(infoIconImg);
-
-	return movieInfoDiv;
+function Movie({ movieInfo }: { movieInfo: TrailerModalProps }) {
+	return (
+		<div className="movie-info relative aspect-video h-full w-52 cursor-pointer overflow-hidden rounded-sm md:w-56 [&:hover_:last-child]:visible">
+			<img
+				src={movieInfo.imageSource}
+				className="h-full w-full"
+				alt="big-buck-bunny.png"
+			/>
+			<div className="absolute bottom-0 left-0 w-full bg-gradient-to-b from-transparent to-zinc-900">
+				<p className="m-2 !line-clamp-1 text-sm font-bold">
+					{movieInfo.title}
+				</p>
+			</div>
+			<div className="invisible absolute left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-30 backdrop-blur-[1px]">
+				<div className="mx-auto flex w-[45%] items-center gap-4">
+					<div className="w-1/2">
+						<img
+							src="/public/icons/info-icon.svg"
+							className="mx-auto rounded-full p-2 hover:backdrop-brightness-75"
+						/>
+					</div>
+					<div
+						className="w-1/2 rounded-full p-2 hover:backdrop-opacity-75"
+						onClick={() => {
+							if (movieInfo.liked) movieInfo.liked = false;
+							else movieInfo.liked = undefined;
+						}}
+					></div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default function App() {
@@ -79,63 +45,20 @@ export default function App() {
 				<Layout />
 				<MediaQuery />
 			</div> */}
-			{/* <div className=""> */}
-			{/* <Wrap>
-					{new Array<TrailerModalProps>(20)
-						.fill({
-							id: 10,
-							description: chance.paragraph(),
-							genre: [Genre.Action, Genre.Crime],
-							imageSource: "/public/images/big-buck-bunny.png",
-							title: "Big Buck Bunny",
-						})
-						.map((movieInfo, index) => (
-							<div
-								className="movie-info relative aspect-video h-full w-52 cursor-pointer overflow-hidden rounded-sm md:w-56 [&:hover_:last-child]:visible"
-								key={index}
-							>
-								<img
-									src={movieInfo.imageSource}
-									className="h-full w-full"
-									alt="big-buck-bunny.png"
-								/>
-								<div className="absolute bottom-0 left-0 w-full bg-gradient-to-b from-transparent to-zinc-900">
-									<p className="m-2 !line-clamp-1 text-sm font-bold">
-										{movieInfo.title}
-									</p>
-								</div>
-								<div className="invisible absolute left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-30 backdrop-blur-[1px]">
-									<div className="mx-auto flex w-[45%] items-center gap-4">
-										<div className="w-1/2">
-											<img
-												src="/public/icons/info-icon.svg"
-												className="mx-auto rounded-full p-2 hover:backdrop-brightness-75"
-											/>
-										</div>
-										<div
-											className="w-1/2 rounded-full p-2 hover:backdrop-opacity-75"
-											onClick={() => {
-												if (movieInfo.liked)
-													movieInfo.liked = false;
-												else
-													movieInfo.liked = undefined;
-											}}
-										></div>
-									</div>
-								</div>
-							</div>
-						))}
-				</Wrap> */}
-			<FixedList
-				elementArray={new Array<TrailerModalProps>(20).fill({
-					id: 10,
-					description: chance.paragraph(),
-					genre: [Genre.Action, Genre.Crime],
-					imageSource: "/public/images/big-buck-bunny.png",
-					title: "Big Buck Bunny",
-				})}
-				node-creator={createMovieNode}
-			/>
+
+			<InfiniteScroller itemCount={20}>
+				{new Array<TrailerModalProps>(300)
+					.fill({
+						id: 10,
+						description: chance.paragraph(),
+						genre: [Genre.Action, Genre.Crime],
+						imageSource: "/public/images/big-buck-bunny.png",
+						title: "Big Buck Bunny",
+					})
+					.map((movieInfo, index) => (
+						<Movie movieInfo={movieInfo} key={index} />
+					))}
+			</InfiniteScroller>
 			{/* </div> */}
 			{/* <Test /> */}
 			{/* <div className="w-5"> */}
--- src/components/InfiniteScroller.tsx
+++ src/components/InfiniteScroller.tsx
@@ -1,202 +1,56 @@
-import { halt } from "@utils/functions";
-import { PropsWithChildren, useEffect, useRef, useState } from "react";
+import {
+	JSX,
+	PropsWithChildren,
+	ReactNode,
+	useEffect,
+	useRef,
+	useState,
+} from "react";
 import { renderToString } from "react-dom/server";
 
-interface FixedListProps<T> {
-	elementArray: T[];
-	"node-creator": (element: T) => HTMLElement;
+/**
+ * @prop gap - it has 2 values which specif their sizes of gap between nodes "low" - 8px, "high" - 16px
+ */
+interface FixedListProps {
+	itemCount: number;
+	children: NonNullable<Iterable<ReactNode>>;
 }
 
-export default function FixedList<T>({
-	"node-creator": nodeCreator,
-	elementArray,
-}: FixedListProps<T>) {
-	console.log(nodeCreator, elementArray);
-
-	const total = new Array<number>(30)
-		.fill(1)
-		.map((number, index) => number + index);
-
-	const limit = 15;
-	const [buffer, setBuffer] = useState<{
-		data: number[];
-	}>({
-		data: total.slice(0, limit),
-	});
-
-	const fixedListRef = useRef<HTMLDivElement>(null);
-
-	async function checkVisible(element: Element) {
-		return await new Promise<IntersectionObserverEntry>((resolve) => {
-			new IntersectionObserver((entries) => {
-				entries.forEach((observerEntry) => {
-					if (observerEntry.isIntersecting) resolve(observerEntry);
-				});
-			}).observe(element);
-		});
-	}
-
-	const fixedListObserver = new IntersectionObserver(
-		(entries) => {
-			entries.forEach((element) => {
-				const node = element.target as HTMLDivElement;
-
-				if (element.isIntersecting) {
-					node.dataset.visible = "true";
-				} else {
-					node.dataset.visible = "false";
-				}
-
-				(async function () {
-					if (
-						node.dataset.last === "true" &&
-						node.dataset.visible === "true"
-					) {
-						let visibleElementNodeList =
-							document.querySelectorAll<HTMLDivElement>(
-								'[data-visible="true"]',
-							);
-
-						const firstElementIntersectionObserverEntry =
-							await checkVisible(
-								visibleElementNodeList[0]
-									.previousElementSibling as Element,
-							);
-
-						let visibleElement = [
-							firstElementIntersectionObserverEntry.target,
-						].concat(
-							Array.from(visibleElementNodeList),
-						) as HTMLDivElement[];
-
-						const newStart = parseInt(
-							visibleElement[0].dataset.elementIndex!,
-						);
-
-						const newEnd = parseInt(
-							visibleElement[visibleElement.length - 1].dataset
-								.elementIndex!,
-						);
-
-						const newElementList = total.slice(newStart, newEnd);
-
-						if (fixedListRef.current?.scrollLeft) {
-							fixedListRef.current.scrollLeft =
-								firstElementIntersectionObserverEntry.target
-									.clientWidth -
-								firstElementIntersectionObserverEntry
-									.intersectionRect.width;
-						}
-
-						setBuffer({
-							data: newElementList.concat(
-								total.slice(
-									newEnd,
-									newEnd + limit - newElementList.length,
-								),
-							),
-						});
-					} else if (
-						node.dataset.first === "true" &&
-						node.dataset.visible === "true"
-					) {
-						await halt();
-
-						const visibleElementNodeList = Array.from(
-							document.querySelectorAll<HTMLDivElement>(
-								'[data-visible="true"]',
-							),
-						);
-
-						const isLastNodeVisible = await checkVisible(
-							visibleElementNodeList.at(-1)!.nextElementSibling!,
-						);
-
-						if (isLastNodeVisible.isIntersecting)
-							visibleElementNodeList.push(
-								isLastNodeVisible.target as HTMLDivElement,
-							);
-
-						const startIndex =
-							parseInt(
-								visibleElementNodeList.at(0)?.dataset
-									.elementIndex!,
-							) - 1;
-
-						const endIndex = parseInt(
-							visibleElementNodeList.at(-1)?.dataset
-								.elementIndex!,
-						);
-
-						const previousStartIndex =
-							limit - visibleElementNodeList.length;
-
-						if (fixedListRef.current?.scrollLeft)
-							fixedListRef.current.scrollLeft = 1000;
-
-						if (
-							previousStartIndex > 0 &&
-							startIndex > previousStartIndex
-						) {
-							const previousElementList = total.slice(
-								startIndex - previousStartIndex,
-								endIndex,
-							);
-							if (previousElementList !== buffer.data)
-								setBuffer({
-									data: previousElementList,
-								});
-						}
-					}
-				})();
-			});
-		},
-		{
-			threshold: 1,
-		},
-	);
-
-	// console.log(buffer.data);
+export default function InfiniteScroller({
+	itemCount,
+	children,
+}: FixedListProps) {
+	const [index, setIndex] = useState(itemCount);
 
 	useEffect(() => {
-		if (fixedListRef.current) {
-			Array.from(fixedListRef.current.children).forEach((element) => {
-				fixedListObserver.observe(element);
+		const observer = new IntersectionObserver((entries) => {
+			entries.forEach((element) => {
+				if (element.isIntersecting) setIndex(index + itemCount);
 			});
-		}
-		return () => {
-			fixedListObserver.disconnect();
-		};
-	}, [buffer, fixedListRef]);
+		});
+		Array.from(document.querySelectorAll('[data-last="true"')).forEach(
+			(element) => {
+				observer.observe(element);
+			},
+		);
+	}, [index]);
 
 	return (
-		<div
-			className="scrollbar-none m-4 flex gap-2 overflow-auto focus-within:outline-dotted focus-within:outline-2"
-			tabIndex={0}
-			ref={fixedListRef}
-		>
-			{buffer.data.map((element, index) => (
-				<div
-					key={index + " " + element}
-					data-element-index={element - 1}
-					data-first={index === 0}
-					data-last={
-						index === limit - 3 ||
-						index === limit - 2 ||
-						index === limit - 1
-					}
-					className={`inline-block ${(() => {
-						if (element % 5 === 0) return "bg-sky-400";
-						if (element % 3 === 0) return "bg-lime-400";
-						if (element % 2 === 0) return "bg-pink-400";
-						return "bg-amber-400";
-					})()} ${element}`}
-				>
-					<p className="aspect-video w-40 text-center text-7xl text-white">
-						{element}
-					</p>
-				</div>
-			))}
+		<div>
+			<div className="scrollbar-none m-4 flex gap-3 overflow-auto focus-within:outline-dotted focus-within:outline-2">
+				{Array.isArray(children) &&
+					Array.from(children)
+						.slice(0, index)
+						.map((element, index, array) => (
+							<div
+								data-last={index > array.length - 6}
+								data-index={index}
+								key={index}
+							>
+								{element}
+							</div>
+						))}
+			</div>
 		</div>
 	);
 }
